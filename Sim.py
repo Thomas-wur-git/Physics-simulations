0c55a26aecdba573253d6e7502445e61276a00ba
Thomas-wur-git <tom.wur@icloud.com>
2025-04-20 02:34:10 -0400

    asdfasdfasdgashdfjskdf

--- Sim.py
+++ Sim.py
@@ -28,29 +28,45 @@ r = 0.5 # Distance from the seat to the center of mass of BEATRICE
 I = 10 # Moment of inertia of BEATRICE (need real mesurments)
 
 TAILLE_BEATRICE = vp.vec(0.2, 1.55, 0.35)
-MASSE_BEATRICE = 5 # 46.72
+MASSE_BEATRICE = 46.72
 
 # Constants you SHALL NOT TOUCH
 
 BLOCK_OFF = -1/2 - TAILLE_BEATRICE.y/2
+art_l = I/MASSE_BEATRICE # Artificial length, of an equivalent point mass system
 
 # // Coordinates
 
+# The life saver:
+# https://web.mit.edu/jorloff/www/chaosTalk/double-pendulum/double-pendulum-en.html
+
 def P1_upt_func():
-    P1["a"] = -(g/l)*vp.sin(P1["x"]) # (-1/(1 + vp.sin(P1["x"] + P2["x"])**2)) * ((g/l)*vp.sin(P1["x"]) + (P2["m"]*g/(l*P1["m"]))*vp.cos(P2["x"])*vp.sin(P2["x"] - P1["x"]))
+    m1 = P1["m"]
+    m2 = P2["m"]
+
+    Num = -g*(2*m1 + m2)*vp.sin(P1["x"]) - m2*g*vp.sin(P1["x"] - 2*P2["x"]) - 2*vp.sin(P1["x"] - P2["x"])*m2*((P2["v"]**2)*art_l + (P1["v"]**2)*l*vp.cos(P1["x"] - P2["x"]))
+    Denum = l*(2*m1 + m2 - m2*vp.cos(2*P1["x"] - 2*P2["x"]))
+    return Num/Denum
+    #P1["a"] = (-1/((l**2)*(m1 + m2))) * (m2*l*art_l*P2["a"]*vp.cos(P2["x"] - P1["x"]) - m2*l*art_l*P2["v"]*vp.sin(P2["x"] - P1["x"]) + (m1 + m2)*g*l*vp.sin(P1["x"]))
 
 def P2_upt_func():
-    P2["a"] = -((r*P2["m"]*g)/I)*vp.sin(P2["x"]) -((P1["m"]*l*P1["a"])/I)*vp.cos(P1["x"] - P2["x"])
+    m1 = P1["m"]
+    m2 = P2["m"]
+
+    Num = 2*vp.sin(P1["x"] - P2["x"])*((P1["v"]**2)*l*(m1 + m2) + g*(m1 + m2)*vp.cos(P1["x"]) + (P2["v"]**2)*art_l*m2*vp.cos(P1["x"] - P2["x"]))
+    Denum = l*(2*m1 + m2 - m2*vp.cos(2*P1["x"] - 2*P2["x"]))
+    return Num/Denum
+	#(-1/(m2*art_l^2)) * (m2*l*art_l*P1["a"]*math.cos(P2["x"] - P1["x"]) + m2*l*art_l*P1["v"]*math.sin(P2["x"] - P1["x"]) + m2*g*art_l*math.sin(P2["x"]))
 
 #P2 = None
 
 obj1 = vp.box(color = vp.color.red, size = vp.vec(.5,.1,1))
-obj2 = vp.box(color = vp.color.yellow, size = TAILLE_BEATRICE, texture = "test.png")
+obj2 = vp.box(color = vp.color.yellow, size = TAILLE_BEATRICE) # texture = "test.png"
 
 P1 = {
     "a": 0,
     "v": 0,
-    "x": 3,
+    "x": 1,
     "m" : 1,
     "obj": obj1,
     "upd_func": P1_upt_func,
@@ -94,22 +110,24 @@ while Running:
     start_tick = vp.clock()
 
     # Updating the block's acceleration
-    P1["upd_func"]()      
-    
+    a1 = P1["upd_func"]()
+    a2 = P2["upd_func"]()
+
+    P1["a"] = a1
+    P2["a"] = a2
+
     P1["v"] += P1["a"]*dt
     P1["x"] += P1["v"]*dt
+
+    P2["v"] += P2["a"]*dt
+    P2["x"] += P2["v"]*dt
 
     angle_1 = P1["x"] - vp.pi/2 # -pi to have 0 at (1,0) on the unit circle
     P1["obj"].pos = l*vp.vec(vp.cos(angle_1),vp.sin(angle_1),0)
     P1["obj"].axis = vp.vec(vp.cos(angle_1 + vp.pi/2), vp.sin(angle_1 + vp.pi/2), 0) * P1["obj"].length
 
-    P2["upd_func"]()
-
-    P2["v"] += P2["a"]*dt
-    P2["x"] += P2["v"]*dt
-
     angle_2 = P2["x"] - vp.pi/2 # -pi to have 0 at (1,0) on the unit circle
-    P2["obj"].pos = (P1["obj"].pos + 0*vp.vec(vp.cos(angle_2),vp.sin(angle_2),0) 
+    P2["obj"].pos = (P1["obj"].pos + vp.vec(vp.cos(angle_2),vp.sin(angle_2),0) 
         #+ r*vp.vec(vp.cos(angle_1 + vp.pi/2), vp.sin(angle_1 + vp.pi/2), 0)
         #+ r*vp.vec(vp.cos(angle_2 + vp.pi/2), vp.sin(angle_2 + vp.pi/2), 0)
     )
